418f3e171c93fd925757126264645c4f4e90e3ef
Andrew Leverette <andrew.leverette@antage.net>
2024-08-10 15:23:29 -0500

    Rename function and add sample tests

--- src/advent_of_code_2015/day_04.clj
+++ src/advent_of_code_2015/day_04.clj
@@ -4,7 +4,7 @@
   (:require [clojure.string :as str]
             [clj-commons.digest :as digest]))
 
-(defn find-hash-that-starts-with
+(defn find-min-number
   "Given a prefix and a base, find the lowest positive number n where
   the MD5 hash of base and some n starts with the prefix."
   [prefix base]
@@ -16,8 +16,8 @@
 
 (defn part-1
   [input]
-  (find-hash-that-starts-with "00000" input))
+  (find-min-number "00000" input))
 
 (defn part-2
   [input]
-  (find-hash-that-starts-with "000000" input))
+  (find-min-number "000000" input))
--- test/advent_of_code_2015_test/day_04_test.clj
+++ test/advent_of_code_2015_test/day_04_test.clj
@@ -1,1 +1,8 @@
-(ns advent-of-code-2015-test.day-04-test)
+(ns advent-of-code-2015-test.day-04-test
+  (:require [clojure.test :refer [is testing deftest]]
+            [advent-of-code-2015.day-04 :refer [find-min-number]]))
+
+(deftest part-1-samples
+  (testing "Puzzle input examples for part 1"
+    (is (= 609043 (find-min-number "00000" "abcdef")))
+    (is (= 1048970 (find-min-number "00000" "pqrstuv")))))
