0ddb1e4836e5d6854099e1534db2de2f22b38bea
Andrew Leverette <andrewleverette@gmail.com>
2024-12-04 09:10:36 -0600

    Day 2 Year 2024 (#2)
    * Add day 2 files
    
    * Add solution for day 2 year 2024
    
    ---------
    
    Co-authored-by: Andrew Leverette <andrew.leverette@antage.net>

--- src/advent_of_code_2024/core.clj
+++ src/advent_of_code_2024/core.clj
@@ -1,8 +1,11 @@
 (ns advent-of-code-2024.core
   (:require
-   [advent-of-code-2024.day-01 :as day01]))
+   [advent-of-code-2024.day-01 :as day01]
+   [advent-of-code-2024.day-02 :as day02]))
 
 (defmulti runner (fn [tag _] tag))
 (defmethod runner :day1.1 [_ input] (day01/part-1 input))
 (defmethod runner :day1.2 [_ input] (day01/part-2 input))
+(defmethod runner :day2.1 [_ input] (day02/part-1 input))
+(defmethod runner :day2.2 [_ input] (day02/part-2 input))
 (defmethod runner :default [_ _] "Invalid day and part combination")
---
+++ src/advent_of_code_2024/day_02.clj
@@ -0,0 +1,56 @@
+(ns advent-of-code-2024.day-02
+  (:require
+   [advent-of-code.enumerate :as enum]
+   [advent-of-code.math :as m]
+   [advent-of-code.parsing :as p]))
+
+(defn- all-increasing-in-range?
+  [deltas]
+  (every? #(<= 1 % 3) deltas))
+
+(defn- all-decreasing-in-range?
+  [report]
+  (every? #(<= -3 % -1) report))
+
+(defn report-is-safe?
+  "Given a report, return true if the report is safe.
+  A report is safe if levels are all increasing or all decreasing
+  and adjacent levels differ by at least 1 and at most 3"
+  [report]
+  (when (seq report)
+    (let [deltas (->> report
+                      (partition 2 1)
+                      (map #(apply - %)))]
+      (or (all-increasing-in-range? deltas)
+          (all-decreasing-in-range? deltas)))))
+
+(defn report-is-safe-with-dampener?
+  "Given a report, return true if the report is safe with dampening.
+  A report is safe with dampening if the report is safe or the report
+  is safe with one level ignored"
+  [report]
+  (when (seq report)
+    (->> report
+         (enum/drop-subranges 1)
+         (cons report)
+         (some report-is-safe?))))
+
+(defn total-safe-reports
+  [safe? reports]
+  (enum/count-when safe? reports))
+
+(defn str->report
+  [s]
+  (map parse-long (re-seq #"\d+" s)))
+
+(defn part-1
+  [input]
+  (->> input
+       (p/parse-input str->report)
+       (total-safe-reports report-is-safe?)))
+
+(defn part-2
+  [input]
+  (->> input
+       (p/parse-input str->report)
+       (total-safe-reports report-is-safe-with-dampener?)))
